9695ec8647129dfa6b16451c0e837d953d687b2d
Florent Monbillard <f.monbillard@gmail.com>
2019-03-12 16:02:47 -0400

    Add verify cert info

--- cert-chain.rb
+++ cert-chain.rb
@@ -9,6 +9,10 @@ if ARGV.length < 1
   exit 1
 end
 
+def verify_cert(verify)
+  "[#{verify ? "\e[32mOK\e[0m" : "\e[31mKO\e[0m"}]"
+end
+
 hostname = ARGV[0]
 port = ARGV[1] ? ARGV[1] : 443
 
@@ -16,10 +20,9 @@ cert_chain = []
 
 ssl_context = OpenSSL::SSL::SSLContext.new
 ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
-ssl_context.verify_callback = lambda do |preverify_ok, cert_store|
-  return false unless preverify_ok # do what usually happens when bad cert
-  cert_chain << cert_store.current_cert
-  true
+ssl_context.verify_callback = lambda do |verify_ok, cert_store|
+  cert_chain << { cert: cert_store.current_cert, verify: verify_ok }
+  verify_ok
 end
 
 cert_store = OpenSSL::X509::Store.new
@@ -29,10 +32,12 @@ ssl_context.cert_store = cert_store
 
 socket = OpenSSL::SSL::SSLSocket.new(TCPSocket.new(hostname, port), ssl_context).tap{|e| e.connect}
 
-cert_chain.reverse.each do |cert|
+cert_chain.reverse.each do |store|
+  cert = store[:cert]
+  verify = store[:verify]
   if socket.peer_cert_chain.include?(cert)
-    puts "\e[32mSent by Server\e[0m: #{cert.subject.to_s(OpenSSL::X509::Name::ONELINE)}"
+    puts "\e[34mSent by Server\e[0m: #{verify_cert(verify)} #{cert.subject.to_s(OpenSSL::X509::Name::ONELINE)}"
   else
-    puts "\e[36mIn Trust Store\e[0m: #{cert.subject.to_s(OpenSSL::X509::Name::ONELINE)}"
+    puts "\e[36mIn Trust Store\e[0m: #{verify_cert(verify)} #{cert.subject.to_s(OpenSSL::X509::Name::ONELINE)}"
   end
 end
